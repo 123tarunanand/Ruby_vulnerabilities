a06fde2673087fa14941780810fe89424ac6d895
Tarun Anand <anandtarun2@gmail.com>
2022-02-04 11:19:06 -0600

    cccchngs

--- sample.rb
+++ sample.rb
@@ -7,3 +7,19 @@ def CWE_23_Path_traversal
   parent = "/path/to/parent_directory"
   Pathname.new(File.join(parent, params[:name]))
 end
+
+
+
+
+
+def CWE_23_Path_traversal_mitigated
+  # params - data received during a http request
+  #
+  # This function takes in a file name and creates a new path in the parent directory
+  #
+
+  parent = "/path/to/parent_directory"
+  if( !params[:name].match?(/[^a-zA-Z0-9]/ ))
+    Pathname.new(File.join(parent, params[:name]))
+  end
+end
