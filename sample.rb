8c3d9cf046ecc039f1a7a34e78271f622fc479a0
Tarun Anand <anandtarun2@gmail.com>
2022-02-04 10:51:44 -0600

    rtemoved rube

--- sample.rb
+++ sample.rb
@@ -1,10 +1,6 @@
-def uppy_upload_path
-  # careful:
-  #
-  #     File.join '/a/b', '/c' => '/a/b/c'
-  #     Pathname.new('/a/b').join('/c') => '/c'
-  #
-  # handle case where uppy.js sets relativePath to "null"
+def CWE_23_Path_traversal
+
+
   if params[:relativePath] && params[:relativePath] != "null"
     Pathname.new(File.join(params[:parent], params[:relativePath]))
   else
