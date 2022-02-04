541cd464e9fc25f7943b2ecc0dc5f31377a286b1
Tarun Anand <anandtarun2@gmail.com>
2022-02-04 10:48:32 -0600

    New rube

--- sample.rb
+++ sample.rb
@@ -1,8 +1,13 @@
-def CWE78_OS_injection()
-    puts "Enter command to execute:"
-    cmd = gets
-    system cmd
+def uppy_upload_path
+  # careful:
+  #
+  #     File.join '/a/b', '/c' => '/a/b/c'
+  #     Pathname.new('/a/b').join('/c') => '/c'
+  #
+  # handle case where uppy.js sets relativePath to "null"
+  if params[:relativePath] && params[:relativePath] != "null"
+    Pathname.new(File.join(params[:parent], params[:relativePath]))
+  else
+    Pathname.new(File.join(params[:parent], params[:name]))
+  end
 end
-
-
-CWE78_OS_injection()
