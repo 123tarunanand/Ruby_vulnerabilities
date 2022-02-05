eed8b9a4cc8aee1b9421161f5d541cf994111cc6
Tarun Anand <anandtarun2@gmail.com>
2022-02-04 21:42:55 -0600

    added js

--- vuln.js
+++ vuln.js
@@ -13,5 +13,9 @@ http.createServer(function(req, res) {
 http.createServer(function(req, res) {
   // Http server that listens to port 8080 and creates a file in the parent directory based on the name parameter
   var parent = parent = "/path/to/parent_directory";
-  var writeStream = fs.createWriteStream(path.join(parent,req.params['name']));
+  var filename = path.join(parent,req.params['name']);
+  if (filename.indexOf(parent) == 0)
+  {
+    var writeStream = fs.createWriteStream(filename);
+  }
 }).listen(8080);
