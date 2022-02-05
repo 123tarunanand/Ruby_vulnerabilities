91563c7cb8fa1dc689b140037d9eda71a40c3a5b
Tarun Anand <anandtarun2@gmail.com>
2022-02-04 20:53:41 -0600

    added

--- vuln.js
+++ vuln.js
@@ -3,5 +3,7 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
   // This opens up the writeable stream to `output`
-  var writeStream = fs.createWriteStream(req.headers);
+
+  var parent = parent = "/path/to/parent_directory";
+  var writeStream = fs.createWriteStream(path.join(parent,req.params['name']));
 }).listen(8080);
